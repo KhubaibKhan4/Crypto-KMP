476d541c7c1ff64a52318f68a480010dfa6ce464
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-10 10:27:50 +0500

    -Content Negociation & Logging impl (#12)

--- composeApp/src/commonMain/kotlin/org/company/app/data/remote/CryptoClient.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/remote/CryptoClient.kt
@@ -1,9 +1,30 @@
 package org.company.app.data.remote
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 object CryptoClient {
     private val client = HttpClient {
-
+        install(ContentNegotiation) {
+            json(
+                json = Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+        install(Logging) {
+            level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    println(message)
+                }
+            }
+        }
     }
 }
