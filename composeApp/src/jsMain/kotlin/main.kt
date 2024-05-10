ae925bb5e6bf7d87589b99dc3bf1f5eb5b5e0837
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-10 11:11:12 +0500

    -Koin Injection (#24)

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -1,12 +1,17 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import org.company.app.App
+import org.company.app.di.appModule
 import org.jetbrains.skiko.wasm.onWasmReady
+import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     onWasmReady {
         CanvasBasedWindow("Crypto-KMP") {
+            startKoin {
+                modules(appModule)
+            }
             App()
         }
     }
