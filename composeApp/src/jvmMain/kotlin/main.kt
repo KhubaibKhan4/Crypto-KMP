a6f1d390be3e01a31549861c94b4579f1e3dea12
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-10 11:12:39 +0500

    -Koin Injection Impl (#26)

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -2,8 +2,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import java.awt.Dimension
 import org.company.app.App
+import org.company.app.di.appModule
+import org.koin.core.context.startKoin
+import java.awt.Dimension
 
 fun main() = application {
     Window(
@@ -11,7 +13,10 @@ fun main() = application {
         state = rememberWindowState(width = 800.dp, height = 600.dp),
         onCloseRequest = ::exitApplication,
     ) {
-        window.minimumSize = Dimension(350, 600)
+        window.minimumSize = Dimension(1280, 720)
+        startKoin {
+            modules(appModule)
+        }
         App()
     }
 }
