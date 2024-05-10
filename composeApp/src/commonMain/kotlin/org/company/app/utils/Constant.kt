da9f8b4e768b184632d8ba80c0c89292632a1d29
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-10 09:57:00 +0500

    -Utils Impl (#2)

---
+++ composeApp/src/commonMain/kotlin/org/company/app/utils/Constant.kt
@@ -0,0 +1,6 @@
+package org.company.app.utils
+
+object Constant {
+    const val BASE_URL = "https://pro-api.coinmarketcap.com/v1/"
+    const val API_KEY = "55d74250-258c-44bb-ba06-57a19719ffa0"
+}
