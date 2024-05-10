250872d4feaa6c7e3a9304e844975c47d9eb8024
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-10 12:43:50 +0500

    -Search Screen Init (#31)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/home/HomeScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -50,6 +49,7 @@ fun HomeScreen(
 ) {
     var isDark by LocalThemeIsDark.current
     var listingData by remember { mutableStateOf<LatestListing?>(null) }
+    var queryText by remember { mutableStateOf("") }
     LaunchedEffect(Unit) {
         viewModel.getLatestListing()
     }
