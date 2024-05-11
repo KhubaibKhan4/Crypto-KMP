529da142c27bb27b70f3bc4fb68246cf81c66c59
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-11 09:44:58 +0500

    -Network Image Setup Impl (#36)

---
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/NetworkImage.kt
@@ -0,0 +1,39 @@
+package org.company.app.presentation.ui.components
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import io.kamel.core.Resource
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+
+@Composable
+fun NetWorkImage(
+    id: Int,
+    modifier: Modifier,
+) {
+    val image: Resource<Painter> =
+        asyncPainterResource("https://s2.coinmarketcap.com/static/img/coins/64x64/$id.png")
+    KamelImage(
+        resource = image,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.FillBounds,
+        onLoading = {
+            CircularProgressIndicator()
+        },
+        onFailure = {
+            Text("image request failed")
+        },
+        animationSpec = tween(
+            durationMillis = 1500,
+            delayMillis = 300,
+            easing = LinearOutSlowInEasing
+        )
+    )
+}
