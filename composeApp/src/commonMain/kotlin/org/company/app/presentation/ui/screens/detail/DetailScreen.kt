a72303159a18aba351739da95b898a08516632ce
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-12 09:33:18 +0500

    -Detail Screen Init (#67)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/detail/DetailScreen.kt
@@ -1,7 +1,29 @@
 package org.company.app.presentation.ui.screens.detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.WbSunny
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 
 class DetailScreen : Screen {
@@ -11,7 +33,48 @@ class DetailScreen : Screen {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailContent() {
-    Text("Detail Content")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Track Coins",
+                        textAlign = TextAlign.Center,
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize
+                    )
+                },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.WbSunny,
+                        contentDescription = "Menu Icon"
+                    )
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Outlined.Notifications,
+                        contentDescription = "Menu Icon"
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .height(49.dp)
+            )
+        }
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
+                .padding(top = it.calculateTopPadding())
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Content")
+        }
+
+    }
 }
