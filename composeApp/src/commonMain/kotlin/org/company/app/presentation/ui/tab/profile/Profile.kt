3157e71d02c729bb48b752e8d8eb6596b6367bde
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-17 09:04:25 +0500

    -Wwallet Tab Init (#109)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/tab/profile/Profile.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/tab/profile/Profile.kt
@@ -1,8 +1,8 @@
-package org.company.app.presentation.ui.tab.wallet
+package org.company.app.presentation.ui.tab.profile
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Wallet
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,18 +14,18 @@ import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import org.company.app.presentation.ui.screens.home.HomeScreen
 
-object Wallet : Tab {
+object Profile : Tab {
     @Composable
     override fun Content() {
-        Text("Wallet")
+        Text("Profile")
     }
 
     override val options: TabOptions
         @Composable
         get() {
-            val icon = rememberVectorPainter(Icons.Default.Wallet)
-            val title by remember { mutableStateOf("Wallet") }
-            val index: UShort = 2u
+            val icon = rememberVectorPainter(Icons.Default.Person)
+            val title by remember { mutableStateOf("Profile") }
+            val index: UShort = 3u
             return TabOptions(index, title, icon)
         }
 }
