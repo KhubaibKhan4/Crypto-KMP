2f523565e0b20f36f4d1411ef617fcac016cf655
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-17 10:43:30 +0500

    -Bottom Nav Impl (#112)

--- composeApp/src/commonMain/kotlin/org/company/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/App.kt
@@ -1,7 +1,51 @@
 package org.company.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocalLibrary
+import androidx.compose.material.icons.filled.MusicVideo
+import androidx.compose.material.icons.filled.Subscriptions
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LocalLibrary
+import androidx.compose.material.icons.outlined.MusicVideo
+import androidx.compose.material.icons.outlined.Subscriptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.navigator.Navigator
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.annotation.ExperimentalCoilApi
@@ -9,8 +53,14 @@ import coil3.compose.setSingletonImageLoaderFactory
 import coil3.request.CachePolicy
 import coil3.request.crossfade
 import coil3.util.DebugLogger
-import org.company.app.presentation.ui.screens.home.HomeScreen
+import org.company.app.presentation.ui.navigation.rails.items.NavigationItem
+import org.company.app.presentation.ui.navigation.rails.navbar.NavigationSideBar
+import org.company.app.presentation.ui.navigation.tab.analytics.Analytics
+import org.company.app.presentation.ui.navigation.tab.home.Home
+import org.company.app.presentation.ui.navigation.tab.profile.Profile
+import org.company.app.presentation.ui.navigation.tab.wallet.Wallet
 import org.company.app.theme.AppTheme
+import org.company.app.theme.LocalThemeIsDark
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
@@ -18,7 +68,7 @@ internal fun App() = AppTheme {
     setSingletonImageLoaderFactory { context ->
         getAsyncImageLoader(context)
     }
-    Navigator(HomeScreen())
+    AppContent()
 }
 
 fun getAsyncImageLoader(context: PlatformContext) =
@@ -27,5 +77,156 @@ fun getAsyncImageLoader(context: PlatformContext) =
         .logger(DebugLogger())
         .memoryCachePolicy(CachePolicy.ENABLED)
         .build()
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun AppContent() {
+    val items = listOf(
+        NavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Default.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+        ),
+        NavigationItem(
+            title = "Shorts",
+            selectedIcon = Icons.Filled.MusicVideo,
+            unselectedIcon = Icons.Outlined.MusicVideo,
+            hasNews = true,
+        ),
+        NavigationItem(
+            title = "Subscriptions",
+            selectedIcon = Icons.Filled.Subscriptions,
+            unselectedIcon = Icons.Outlined.Subscriptions,
+            hasNews = false,
+        ),
+        NavigationItem(
+            title = "Library",
+            selectedIcon = Icons.Filled.LocalLibrary,
+            unselectedIcon = Icons.Outlined.LocalLibrary,
+            hasNews = false,
+        ),
+    )
+    val windowClass = calculateWindowSizeClass()
+    val showNavigationRail = windowClass.widthSizeClass != WindowWidthSizeClass.Compact
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    TabNavigator(Home) { tabNavigator ->
+        Scaffold(bottomBar = {
+            if (!showNavigationRail) {
+                BottomNavigation(
+                    modifier = Modifier.fillMaxWidth().windowInsetsPadding(WindowInsets.ime),
+                    backgroundColor = MaterialTheme.colorScheme.background,
+                    contentColor = contentColorFor(Color.Red),
+                    elevation = 8.dp
+                ) {
+                    TabItem(Home)
+                    TabItem(Analytics)
+                    TabItem(Wallet)
+                    TabItem(Profile)
+                }
+            }
+        }) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(
+                    bottom = it.calculateBottomPadding(),
+                    start = if (showNavigationRail) 80.dp else 0.dp
+                )
+            ) {
+                CurrentTab()
+            }
+        }
+    }
+    if (showNavigationRail) {
+        NavigationSideBar(
+            items = items,
+            selectedItemIndex = selectedItemIndex,
+            onNavigate = {
+                selectedItemIndex = it
+            }
+        )
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(start = 80.dp)
+        ) {
+            when (selectedItemIndex) {
+                0 -> {
+
+                }
+
+                1 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(Analytics)
+                    }
+                }
+
+                2 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(Wallet)
+                    }
+                }
+
+                3 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(Profile)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun RowScope.TabItem(tab: Tab) {
+    val isDark by LocalThemeIsDark.current
+    val tabNavigator = LocalTabNavigator.current
+    BottomNavigationItem(
+        modifier = Modifier.background(MaterialTheme.colorScheme.surface)
+            .height(58.dp).clip(RoundedCornerShape(16.dp)),
+        selected = tabNavigator.current == tab,
+        onClick = {
+            tabNavigator.current = tab
+        },
+        icon = {
+            tab.options.icon?.let { painter ->
+                Icon(
+                    painter,
+                    contentDescription = tab.options.title,
+                    tint = if (tabNavigator.current == tab) Color.Red else if (isDark) Color.White else Color.Black
+                )
+            }
+        },
+        label = {
+            tab.options.title.let { title ->
+                Text(
+                    title,
+                    fontSize = 12.sp,
+                    color = if (tabNavigator.current == tab) Color.Red else if (isDark) Color.White else Color.Black
+                )
+            }
+        },
+        enabled = true,
+        alwaysShowLabel = true,
+        interactionSource = MutableInteractionSource(),
+        selectedContentColor = Color.Red,
+        unselectedContentColor = Color.Black
+    )
+}
 
 internal expect fun openUrl(url: String?)
--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/navigation/rails/navbar/NavigationSideBar.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/navigation/rails/navbar/NavigationSideBar.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Badge
@@ -21,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import org.company.app.presentation.ui.navigation.rails.items.NavigationItem
 
@@ -37,6 +40,8 @@ fun NavigationSideBar(
                 Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
             }
         },
+        modifier = Modifier.height(400.dp)
+            .clip(RoundedCornerShape(40.dp))
     ) {
         Column(
             modifier = Modifier.fillMaxHeight(),
