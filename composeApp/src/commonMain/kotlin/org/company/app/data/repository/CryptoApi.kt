b3491771a011394e8311e22e838ed3b94b0205f6
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-16 09:26:37 +0500

    -Get All News Impl (#158)

--- composeApp/src/commonMain/kotlin/org/company/app/data/repository/CryptoApi.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/repository/CryptoApi.kt
@@ -1,7 +1,9 @@
 package org.company.app.data.repository
 
 import org.company.app.domain.model.crypto.LatestListing
+import org.company.app.domain.model.news.NewsList
 
 interface CryptoApi {
     suspend fun getLatestListing(): LatestListing
+    suspend fun getAllNews(): List<NewsList>
 }
