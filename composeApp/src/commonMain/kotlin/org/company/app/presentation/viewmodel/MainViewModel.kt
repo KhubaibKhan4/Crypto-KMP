7bf0e49b3e663b93b3112899449d437d8bc22e44
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-16 09:30:58 +0500

    -Get News ViewModel Impl (#160)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/viewmodel/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/viewmodel/MainViewModel.kt
@@ -7,12 +7,17 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.company.app.domain.model.crypto.LatestListing
+import org.company.app.domain.model.news.NewsList
 import org.company.app.domain.repository.Repository
 import org.company.app.domain.usecase.ResultState
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
     private val _latestListing = MutableStateFlow<ResultState<LatestListing>>(ResultState.LOADING)
     var latestListing: StateFlow<ResultState<LatestListing>> = _latestListing.asStateFlow()
+
+    private val _allNews = MutableStateFlow<ResultState<List<NewsList>>>(ResultState.LOADING)
+    var allNews: StateFlow<ResultState<List<NewsList>>> = _allNews.asStateFlow()
+
 
     fun getLatestListing() {
         viewModelScope.launch {
@@ -26,4 +31,15 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
         }
     }
 
+    fun getAllNews(){
+        viewModelScope.launch {
+            _allNews.value = ResultState.LOADING
+            try {
+                val response = repository.getAllNews()
+                _allNews.value = ResultState.SUCCESS(response)
+            }catch (e:Exception){
+                _allNews.value = ResultState.ERROR(e.message.toString())
+            }
+        }
+    }
 }
