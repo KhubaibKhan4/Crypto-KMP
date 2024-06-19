fbeb66101cc3c95bc607798928c8da5b6651d06d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-19 10:06:06 +0500

    -News Category Repo Impl (#175)

--- composeApp/src/commonMain/kotlin/org/company/app/data/repository/CryptoApi.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/repository/CryptoApi.kt
@@ -1,9 +1,11 @@
 package org.company.app.data.repository
 
+import org.company.app.domain.model.categories.NewsCategoriesItem
 import org.company.app.domain.model.crypto.LatestListing
 import org.company.app.domain.model.news.NewsList
 
 interface CryptoApi {
     suspend fun getLatestListing(): LatestListing
     suspend fun getAllNews(): NewsList
+    suspend fun getNewsCategories(): List<NewsCategoriesItem>
 }
--- composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
@@ -2,6 +2,7 @@ package org.company.app.domain.repository
 
 import org.company.app.data.remote.CryptoClient
 import org.company.app.data.repository.CryptoApi
+import org.company.app.domain.model.categories.NewsCategoriesItem
 import org.company.app.domain.model.crypto.LatestListing
 import org.company.app.domain.model.news.NewsList
 
@@ -12,5 +13,9 @@ class Repository: CryptoApi {
 
     override suspend fun getAllNews(): NewsList {
         return CryptoClient.getAllNews()
+    }
+
+    override suspend fun getNewsCategories(): List<NewsCategoriesItem> {
+        return CryptoClient.getNewsCategories()
     }
 }
